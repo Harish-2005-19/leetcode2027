4fce9720c8914c31c8a27840b40568d6c07dd2ba
Harish-2005-19 <harishts2005@gmail.com>
2026-03-24 13:40:21 +0530

    Time: 7 ms (94.66%), Space: 47.3 MB (12.63%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.java
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -1,12 +1,13 @@
- class MyLinkedList {
+class MyLinkedList {
 
     class Node {
         int val;
-        Node next;
+        Node next, prev;
 
         Node(int val) {
             this.val = val;
             next = null;
+            prev = null;
         }
     }
     Node head, tail;
@@ -19,7 +20,6 @@
     public int get(int index) {
         if (index < 0 || index >= c)
             return -1;
-
         Node temp = head;
         for (int i = 0; i < index; i++) {
             temp = temp.next;
@@ -28,49 +28,49 @@
     }
     public void addAtHead(int val) {
         Node nn = new Node(val);
-
         if (head == null) {
             head = nn;
             tail = nn;
         } else {
             nn.next = head;
+            head.prev = nn;
             head = nn;
         }
         c++;
     }
+
     public void addAtTail(int val) {
         Node nn = new Node(val);
 
-        if (head == null) {
+        if (tail == null) {
             head = nn;
             tail = nn;
         } else {
             tail.next = nn;
+            nn.prev = tail;
             tail = nn;
         }
-        c++;  
+        c++;
     }
     public void addAtIndex(int index, int val) {
-
         if (index < 0 || index > c)
             return;
-
         if (index == 0) {
             addAtHead(val);
             return;
         }
-
         if (index == c) {
             addAtTail(val);
             return;
         }
-        Node nn = new Node(val);
         Node temp = head;
-
         for (int i = 0; i < index - 1; i++) {
             temp = temp.next;
         }
+        Node nn = new Node(val);
         nn.next = temp.next;
+        nn.prev = temp;
+        temp.next.prev = nn;
         temp.next = nn;
         c++;
     }
@@ -79,19 +79,24 @@
             return;
         if (index == 0) {
             head = head.next;
-
-            if (head == null)
+            if (head != null)
+                head.prev = null;
+            else
                 tail = null;
             c--;
             return;
         }
         Node temp = head;
-        for (int i = 0; i < index - 1; i++) {
+        for (int i = 0; i < index; i++) {
             temp = temp.next;
         }
-        temp.next = temp.next.next;
-        if (temp.next == null)
-            tail = temp;
+        if (temp.next != null)
+            temp.next.prev = temp.prev;
+        else
+            tail = temp.prev;
+        if (temp.prev != null)
+            temp.prev.next = temp.next;
+
         c--;
     }
 }
