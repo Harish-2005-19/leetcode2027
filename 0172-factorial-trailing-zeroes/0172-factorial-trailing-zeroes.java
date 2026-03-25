c801574bbdac43deef329b421425d6ea8568bdbf
Harish-2005-19 <harishts2005@gmail.com>
2026-03-25 10:42:40 +0530

    Time: 0 ms (100%), Space: 42.3 MB (81.95%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int trailingZeroes(int n) {
+       int x =0;
+       while(n>0)
+       {
+            n=n/5;
+            x=x+n;
+       }
+       return x;
+    }
+}
