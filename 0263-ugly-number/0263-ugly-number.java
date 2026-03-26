487942831fa24c52037e632d123a7574af866130
Harish-2005-19 <harishts2005@gmail.com>
2026-03-26 15:33:59 +0530

    Time: 1 ms (99.25%), Space: 42.7 MB (46.04%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0) return false;
+        int arr[] = {2,3,5};
+        for(int i=0;i<3;i++)
+        {
+            while(n%arr[i]==0)
+            {
+                n=n/arr[i];
+            }
+        }
+
+        return n==1;
+    }
+}
