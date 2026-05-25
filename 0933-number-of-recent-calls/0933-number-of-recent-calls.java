b1435fdb9c0792ca2d221a1b958b039f5de8a67b
Harish T S <harishts2005@gmail.com>
2026-05-25 10:23:05 +0530

    Time: 22 ms (55.68%), Space: 59.7 MB (23.04%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,23 +1,24 @@
-class RecentCounter {
+import java.util.*;
+import java.util.LinkedList;
+class RecentCounter{
     Queue<Integer> q;
-
-
-    public RecentCounter() {
-        q=new LinkedList<>();
+    public RecentCounter(){
+        q= new LinkedList<>();
     }
-    
-    public int ping(int t) {
-        q.add(t);
-        while(!q.isEmpty()&& t-3000>q.peek()){
+    public int ping(int t){
+        q.offer(t);
+        while(!q.isEmpty() && q.peek()<t-3000){
             q.poll();
         }
         return q.size();
-        
     }
 }
-
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * RecentCounter obj = new RecentCounter();
- * int param_1 = obj.ping(t);
- */
+public class Recentqueue {
+    public static void main(String[] args){
+        RecentCounter rc = new RecentCounter();
+        System.out.println(rc.ping(1));
+        System.out.println(rc.ping(100));
+        System.out.println(rc.ping(3001));
+        System.out.println(rc.ping(3002));
+    }
+}
