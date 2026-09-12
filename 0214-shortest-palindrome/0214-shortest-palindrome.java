a82e51f0f2f9f076f71f943d6f4b236008a00c71
Harish T S <harishts2005@gmail.com>
2026-09-12 11:17:12 +0530

    Time: 511 ms (5.21%), Space: 47.5 MB (7.53%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,37 +1,17 @@
 class Solution {
-
     public String shortestPalindrome(String s) {
-        String temp = s + "#" + new StringBuilder(s).reverse().toString();
-        int[] table = getTable(temp);
 
-        return new StringBuilder(s.substring(table[table.length - 1]))
-                .reverse().toString() + s;
-    }
+        String reversed = new StringBuilder(s).reverse().toString();
 
-    public int[] getTable(String s) {
-        int[] table = new int[s.length()];
-        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
 
-        for (int i = 1; i < s.length(); i++) {
+            if (s.substring(0, s.length() - i).equals(reversed.substring(i, reversed.length()))) {
 
-            if (s.charAt(index) == s.charAt(i)) {
-                table[i] = table[i - 1] + 1;
-                index++;
-            } else {
-                index = table[i - 1];
+                return new StringBuilder(reversed.substring(0,i)).append(s).toString();
 
-                while (index > 0 && s.charAt(index) != s.charAt(i)) {
-                    index = table[index - 1];
-                }
-
-                if (s.charAt(index) == s.charAt(i)) {
-                    index++;
-                }
-
-                table[i] = index;
             }
-        }
 
-        return table;
+        }
+        return "";
     }
 }
